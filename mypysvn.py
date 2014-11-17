e7aa80b85a7dbe3eaebd1b746d82f56a96e47cc0
Rohan Poojary <rpoojary@Rohans-MBP.attlocal.net>
2014-11-16 20:59:15 -0600

    Pysvn Implementation

---
+++ mypysvn.py
@@ -0,0 +1,36 @@
+import pysvn 
+import re 
+import argparse
+
+def set_default_username(username):
+    return username
+
+def set_default_password(password):
+    return password
+
+def get_log_message():
+    return True ,log_message
+
+def ssl_server_trust_prompt(trust_dict):
+    return True, 3, True 
+
+def get_login(realm, username, may_save):
+    return True,username,password,True
+
+parser = argparse.ArgumentParser("Testing PySVN")
+parser.add_argument('-u', '--username', help= 'SVN Username')
+parser.add_argument('-p', '--password', help= 'SVN Password')
+
+args = parser.parse_args()
+username = args.username
+password = args.password
+client = pysvn.Client()
+
+client.set_default_username(username)
+client.set_default_password(password)
+client.callback_ssl_server_trust_prompt = ssl_server_trust_prompt
+client.callback_get_login = get_login
+client.callback_get_log_message = get_log_message
+
+
+
